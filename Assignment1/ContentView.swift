1c87f750b3f5172f3df60cd6c081a1308028f720
Emily Coggins <139669664+embcogg@users.noreply.github.com>
2024-03-13 20:33:07 -0500

    EmojiCounter
    Final Commit

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -7,15 +7,92 @@
 
 import SwiftUI
 
+class EmojiCounter: ObservableObject {
+    @Published var count: Int
+    
+    init(initialCount: Int) {
+        self.count = initialCount
+    }
+    
+    func increase() {
+        count += 1
+    }
+    
+    func decrease() {
+        count -= 1
+    }
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @StateObject var tiltLaughEmoji = EmojiCounter(initialCount: 3)
+    @StateObject var winkEmoji = EmojiCounter(initialCount: -2)
+    @StateObject var smileEmoji = EmojiCounter(initialCount: 2)
+    @StateObject var starEmoji = EmojiCounter(initialCount: 1)
+    @StateObject var laughEmoji = EmojiCounter(initialCount: -1)
+    
+    var body: some View{
+        NavigationView{
+            VStack(spacing: 6){
+                emojiRow(emoji: "🤣", viewModel: tiltLaughEmoji)
+                emojiRow(emoji: "😜", viewModel: winkEmoji)
+                emojiRow(emoji: "😀", viewModel: smileEmoji)
+                emojiRow(emoji: "🤩", viewModel: starEmoji)
+                emojiRow(emoji: "😂", viewModel: laughEmoji)
+                
+                Spacer()
+            }
+            
+            .padding()
+            .background(Color(.white))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Emoji Counter")
+                        .font(.title.bold())
+                        .foregroundColor(.red)
+                }
+            }
         }
-        .padding()
+    }
+    func emojiRow(emoji: String, viewModel: EmojiCounter) -> some View {
+        HStack {
+            Text(emoji)
+                .font(.system(size: 40))
+            
+            Spacer()
+            
+            Text("Counter: \(viewModel.count)")
+                .font(.title2)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            Button(action: {
+                viewModel.increase()
+            }) {
+                Text("+")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(Color.red)
+                    .overlay(RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.red, lineWidth: 1))
+            }
+            
+            Button(action: {
+                viewModel.increase()
+            }) {
+                Text("-")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(Color.red)
+                    .overlay(RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.red, lineWidth: 1))
+            }
+        }
+        .frame(height: 55)
+        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+        .cornerRadius(8)
+        
     }
 }
 
